68a22a131ca8400c6c4dbda96d49ad02503f70ab
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2014-09-19 17:27:05 +0200

    Moved common utility functions to own file

---
+++ UtilityFunctions.sh
@@ -0,0 +1,29 @@
+#Collections of common operations
+
+function TimeToSeconds(){
+    local T=$1; shift
+    echo $((10#${T:0:2} * 3600 + 10#${T:3:2} * 60 + 10#${T:6:2})) 
+}
+
+function MinimumOfArray(){
+    local MIN=$1; shift
+    while [ "$1" != "" ]; do
+	if [ $(echo "$1 $MIN" | awk '{if($1<$2){print 1}else{print 0}}') -eq 1 ]; then
+	    MIN=$1
+	fi
+	shift
+    done
+    echo "$MIN"
+}
+
+function FindPositionOfFirstMinimumOfArray(){
+    local ARRAY_TMP=("$@")
+    local ARRAY=("$@")
+    local MIN=$(MinimumOfArray "${ARRAY_TMP[@]}")
+    for (( i=0; i<${#ARRAY[@]}; i++ )); do
+	if [ "${ARRAY[$i]}" = "${MIN}" ]; then
+	    echo $i;
+	    break
+	fi
+    done
+}
