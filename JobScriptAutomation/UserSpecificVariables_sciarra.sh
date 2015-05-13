7b9ae21db1fc5c64ffc82ea91cbf8913801409c2
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2015-05-13 17:13:21 +0200

    Adapted code to staggered case

--- JobScriptAutomation/UserSpecificVariables_sciarra.sh
+++ JobScriptAutomation/UserSpecificVariables_sciarra.sh
@@ -1,14 +1,30 @@
 # Paths on LOEWE using CL2QCD
 USER_MAIL="sciarra@th.physik.uni-frankfurt.de"
 HMC_BUILD_PATH="clhmc/build/RefExec"
-SIMULATION_PATH="WilsonProject"
 HOME_DIR="/data01/hfftheo/sciarra" 
 WORK_DIR="/scratch/hfftheo/sciarra" 
-HMC_FILENAME="hmc_ref"
-HMC_GLOBALPATH="${HOME}/$HMC_BUILD_PATH/$HMC_FILENAME"
-INPUTFILE_NAME="hmc.input"
-JOBSCRIPT_PREFIX="job.hmc.cl2qcd.loewe"
-OUTPUTFILE_NAME="hmc_output"
-THERMALIZED_CONFIGURATIONS_PATH="$HOME_DIR/$SIMULATION_PATH/Thermalized_Configurations"
 GPU_PER_NODE=4
 JOBSCRIPT_LOCALFOLDER="JobScripts"
+
+if [ $WILSON = "TRUE" ]; then
+    SIMULATION_PATH="WilsonProject"
+    HMC_FILENAME="hmc_ref"
+    INPUTFILE_NAME="hmc.input"
+    JOBSCRIPT_PREFIX="job.hmc.cl2qcd."
+    OUTPUTFILE_NAME="hmc_output"
+fi
+
+if [ $STAGGERED = "TRUE" ]; then
+    SIMULATION_PATH="StaggeredNf3Test"
+    HMC_FILENAME="rhmc_ref"
+    INPUTFILE_NAME="rhmc.input"
+    JOBSCRIPT_PREFIX="job.rhmc.cl2qcd."
+    OUTPUTFILE_NAME="rhmc_output"
+    RATIONAL_APPROXIMATIONS_PATH="$HOME_DIR/$SIMULATION_PATH/Rational_Approximations"
+    APPROX_HEATBATH_NAME="Approx_Heatbath"
+    APPROX_MD_NAME="Approx_MD"
+    APPROX_METROPOLIS_NAME="Approx_Metropolis"
+fi
+
+HMC_GLOBALPATH="${HOME}/$HMC_BUILD_PATH/$HMC_FILENAME"
+THERMALIZED_CONFIGURATIONS_PATH="$HOME_DIR/$SIMULATION_PATH/Thermalized_Configurations"
