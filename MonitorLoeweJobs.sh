7926c7e070688a5accbfeb1611324e0dcc39ee54
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2016-02-26 12:03:52 +0100

    Redesigned MonitorLoeweJobs.sh script to speed it up

--- MonitorLoeweJobs.sh
+++ MonitorLoeweJobs.sh
@@ -10,31 +10,33 @@ function ParseCommandLineOptions(){
 
     while [ "$1" != "" ]; do
         case $1 in
-        -h | --help )
-            printf "\n\e[0;32m"
-            echo "Call the script $0 with the following optional arguments:"
-            echo "  -h | --help"
-            echo "  -u | --user       ->    user for which jobs should be displayed (DEFAULT = $(whoami))"
-            echo "  -a | --allUsers   ->    display jobs information for all users"
-            echo "  -l | --local      ->    display information for jobs submitted from a folder whose path match the present directory"
-            if [ $CLUSTER_NAME = "LCSC" ]; then
-                echo "  -n | --nodeUsage  ->    display information ONLY about which nodes are used"
-                echo "  -g | --groupBetas ->    display partial information grouping betas used"
-            fi
-            echo -e "\n\e[0;35mNOTE: If the option -a is given, then the -u one is ignored!\e[0m"
-            printf "\n\e[0m"
-            exit
-            shift;;
-        -u=* | --user=* )    SELECTED_USER=${1#*=}; shift ;;
-        -a | --allUsers )    DISPLAY_ALL_JOBS="TRUE"; shift ;;
-	    -l | --local )       MUTUALLYEXCLUSIVEOPTS_PASSED+=( "--local" ); LOCAL_JOBS="TRUE"; shift;;
-	    -n | --nodeUsage )   MUTUALLYEXCLUSIVEOPTS_PASSED+=( "--nodeUsage" ); NODE_USAGE="TRUE"; shift;;
-	    -g | --groupBetas )  MUTUALLYEXCLUSIVEOPTS_PASSED+=( "--groupBetas" ); GROUP_BETAS="TRUE"; shift;;
-        * ) printf "\n\e[0;31mError parsing the options! Aborting...\n\n\e[0m" ; exit -1 ;;
+            -h | --help )
+                printf "\n\e[0;32m"
+                echo "Call the script $0 with the following optional arguments:"
+                echo "  -h | --help"
+                echo "  -u | --user       ->    user for which jobs should be displayed (DEFAULT = $(whoami))"
+                echo "  -a | --allUsers   ->    display jobs information for all users (on $CLUSTER_PARTITION partition)"
+                echo "  -l | --local      ->    display information for jobs submitted from a folder whose path match the present directory"
+                if [ $CLUSTER_NAME = "LCSC" ]; then
+                    echo "  -n | --nodeUsage  ->    display information ONLY about which nodes are used"
+                    echo "  -g | --groupBetas ->    display partial information grouping betas used"
+                    echo "                          WARNING: it is useful when a single simulation per job is run,"
+                    echo "                                   otherwise the output is incomplete!"
+                fi
+                echo -e "\n\e[0;35mNOTE: If the option -a is given, then the -u one is ignored!\e[0m"
+                printf "\n\e[0m"
+                exit
+                shift;;
+            -u=* | --user=* )    SELECTED_USER=${1#*=}; shift ;;
+            -a | --allUsers )    DISPLAY_ALL_JOBS="TRUE"; shift ;;
+	        -l | --local )       LOCAL_JOBS="TRUE"; shift;;
+	        -n | --nodeUsage )   MUTUALLYEXCLUSIVEOPTS_PASSED+=( "--nodeUsage" ); NODE_USAGE="TRUE"; DISPLAY_STANDARD_LIST='FALSE'; shift;;
+	        -g | --groupBetas )  MUTUALLYEXCLUSIVEOPTS_PASSED+=( "--groupBetas" ); GROUP_BETAS="TRUE"; DISPLAY_STANDARD_LIST='FALSE'; shift;;
+            * ) printf "\n\e[0;31mError parsing the options! Aborting...\n\n\e[0m" ; exit -1 ;;
         esac
     done
 
-    if { [ $LOCAL_JOBS = 'TRUE' ] || [ $GROUP_BETAS = 'TRUE' ]; } && [ $CLUSTER_NAME != "LCSC" ]; then
+    if { [ $NODE_USAGE = 'TRUE' ] || [ $GROUP_BETAS = 'TRUE' ]; } && [ $CLUSTER_NAME != "LCSC" ]; then
         printf "\n\e[0;31mError parsing the options (see --help)! Aborting...\n\n\e[0m"
         exit -1
     fi
@@ -42,97 +44,119 @@ function ParseCommandLineOptions(){
     if [ ${#MUTUALLYEXCLUSIVEOPTS_PASSED[@]} -gt 1 ]; then
 	    printf "\n\e[0;31m The options\n\n\e[1m"
 	    for OPT in "${MUTUALLYEXCLUSIVEOPTS[@]}"; do
-	        #echo "  $OPT"
 	        printf "  %s\n" "$OPT"
 	    done
 	    printf "\n\e[0;31m are mutually exclusive and must not be combined! Aborting...\n\n\e[0m"
 	    exit -1
     fi
-        
+    
 }
 
-function ExtractParameterFromJobInformations(){
-    local JOB_ID_NUMBER="$1"
-    local PARAMETER_NAME="$2"
-    PARAMETER_VALUE=$(scontrol show job $JOBID | sed -n 's@.*'${PARAMETER_NAME}'=\([^[:space:]]*\).*@\1@p')
-    echo "$PARAMETER_VALUE"
+#Unused function left here in case in future some more information is needed (for which squeue has not a format option)
+function ExtractParametersFromJobInformation(){
+    local JOB_ID_NUMBER="$1"; shift
+    local PARAMETERS_NAME=("$@")
+    local SCONTROL_OUTPUT="$(scontrol show job $JOBID)"
+    for PARAM in "${PARAMETERS_NAME[@]}"; do
+        JOBINFO["$PARAM"]=$(sed -n 's@.*'${PARAM}'=\([^[:space:]]*\).*@\1@p' <<< "$SCONTROL_OUTPUT")
+    done && unset -v 'PARAM'
 }
-
 
 #-----------------------------------------------------------------------------------------------#
-
+#Variable for the script
 CLUSTER_NAME="LOEWE"
-[ "$(hostname)" = "lxlcsc0001" ] && CLUSTER_NAME="LCSC"
+CLUSTER_PARTITION="gpu"
+if [ "$(hostname)" = "lxlcsc0001" ]; then
+    CLUSTER_NAME="LCSC"
+    CLUSTER_PARTITION="lcsc"
+fi
 MUTUALLYEXCLUSIVEOPTS=( "-n | --nodeUsage" "-g | --groupBetas" "-l | --local" )
 MUTUALLYEXCLUSIVEOPTS_PASSED=( )
 SELECTED_USER="$(whoami)"
-TEMPORARY_FILE_FOR_COLOURED_JOBINFORMATION="temporaryFileForColouredListstatus.dat"
 DISPLAY_ALL_JOBS="FALSE"
 NODE_USAGE="FALSE"
 GROUP_BETAS="FALSE"
 LOCAL_JOBS="FALSE"
+DISPLAY_STANDARD_LIST='TRUE'
 
+#Format codes for squeue command in order to get specific information
+declare -A SQUEUE_FORMAT_CODE
+SQUEUE_FORMAT_CODE["JobId"]="%i"
+SQUEUE_FORMAT_CODE["Name"]="%j"
+SQUEUE_FORMAT_CODE["JobState"]="%T"
+SQUEUE_FORMAT_CODE["[[:space:]]NodeList"]="%N"
+SQUEUE_FORMAT_CODE["SubmitTime"]="%V"
+SQUEUE_FORMAT_CODE["TimeLimit"]="%l"
+SQUEUE_FORMAT_CODE["StartTime"]="%S"
+SQUEUE_FORMAT_CODE["RunTime"]="%M"
+SQUEUE_FORMAT_CODE["EndTime"]="%e"
+SQUEUE_FORMAT_CODE["WorkDir"]="%Z"
+SQUEUE_FORMAT_CODE["NumNodes"]="%D"
+#Space before NodeList is crucial if one wants to parse the output of scontrol show job because there are also ReqNodeList and ExcNodeList
+SQUEUE_FORMAT_CODE_ORDER=("JobId" "Name" "JobState" "[[:space:]]NodeList" "SubmitTime" "TimeLimit" "StartTime" "RunTime" "EndTime" "WorkDir" "NumNodes")
+for LABEL in "${SQUEUE_FORMAT_CODE_ORDER[@]}"; do
+    SQUEUE_FORMAT_CODE_STRING="${SQUEUE_FORMAT_CODE_STRING}@${SQUEUE_FORMAT_CODE[$LABEL]}"
+done
 
+#-----------------------------------------------------------------------------------------------#
+#Common part of script options
 ParseCommandLineOptions $@
 
-if [ $DISPLAY_ALL_JOBS = "FALSE" ]; then
-    JOBID_ARRAY=( $(squeue | awk -v username="$SELECTED_USER" 'NR>1{if($4 == username){print $1}}') )
+#Get information via squeue and in case filter jobs -> ATTENTION: Double quoting here is CRUCIAL (to respect endlines)!!
+if [ $DISPLAY_ALL_JOBS = 'TRUE' ]; then
+    SQUEUE_OUTPUT="$(squeue --noheader -p $CLUSTER_PARTITION -o ${SQUEUE_FORMAT_CODE_STRING:1})"
 else
-    JOBID_ARRAY=( $(squeue | awk 'NR>1{print $1}') )
+    SQUEUE_OUTPUT="$(squeue --noheader -u $SELECTED_USER -o "${SQUEUE_FORMAT_CODE_STRING:1}")"
 fi
-JOBNAME=()
-JOBSTATUS=()
-JOBSTARTTIME=()
-JOBENDTIME=()
-JOBSUBTIME=()
-JOBSUBFROM=()
-JOBNUMNODES=()
-JOBFIRSTNODE=()
-JOBWALLTIME=()
-JOBRUNTIME=()
 
-for JOBID in ${JOBID_ARRAY[@]}; do
+if [ $LOCAL_JOBS = 'TRUE' ]; then
+    SQUEUE_OUTPUT="$(grep --color=never "${PWD}" <<< "$SQUEUE_OUTPUT")"
+fi
 
-    JOBNAME+=( $(ExtractParameterFromJobInformations $JOBID "Name") )
-    JOBSTATUS+=( $(ExtractParameterFromJobInformations $JOBID "JobState") )
-    JOBSTARTTIME+=( $(ExtractParameterFromJobInformations $JOBID "StartTime") )
-    JOBSUBTIME+=( $(ExtractParameterFromJobInformations $JOBID "SubmitTime") )
-    JOBENDTIME+=( $(ExtractParameterFromJobInformations $JOBID "EndTime") )
-    JOBSUBFROM+=( $(ExtractParameterFromJobInformations $JOBID "WorkDir") )
-    JOBNUMNODES+=( $(ExtractParameterFromJobInformations $JOBID "NumNodes") )
-    JOBWALLTIME+=( $(ExtractParameterFromJobInformations $JOBID "TimeLimit") )
-    JOBRUNTIME+=( $(ExtractParameterFromJobInformations $JOBID "RunTime") )
-    #I do not know if this work for jobs on several nodes
-    JOBFIRSTNODE+=( $(ExtractParameterFromJobInformations $JOBID "[[:space:]]NodeList") ) #Space before NodeList is crucial because there are also ReqNodeList and ExcNodeList
+#Split squeue output and prepare table layout
+JOB_ID=(                $(cut -d'@' -f1  <<< "$SQUEUE_OUTPUT") )
+JOB_NAME=(              $(cut -d'@' -f2  <<< "$SQUEUE_OUTPUT") )
+JOB_STATUS=(            $(cut -d'@' -f3  <<< "$SQUEUE_OUTPUT") )
+JOB_NODELIST=(          $(cut -d'@' -f4  <<< "$SQUEUE_OUTPUT") )
+JOB_SUBMISSION_TIME=(   $(cut -d'@' -f5  <<< "$SQUEUE_OUTPUT") )
+JOB_WALLTIME=(          $(cut -d'@' -f6  <<< "$SQUEUE_OUTPUT") )
+JOB_START_TIME=(        $(cut -d'@' -f7  <<< "$SQUEUE_OUTPUT") )
+JOB_RUNTIME=(           $(cut -d'@' -f8  <<< "$SQUEUE_OUTPUT") )
+JOB_END_TIME=(          $(cut -d'@' -f9  <<< "$SQUEUE_OUTPUT") )
+JOB_SUBMISSION_FOLDER=( $(cut -d'@' -f10 <<< "$SQUEUE_OUTPUT") )
+JOB_NUM_NODES=(         $(cut -d'@' -f11 <<< "$SQUEUE_OUTPUT") )
 
-done
+if [ $CLUSTER_NAME = 'LOEWE' ]; then
+    HOME_DIR=${HOME}
+    WORK_DIR="/home/hfftheo/$(whoami)"
+    DATA1_DIR="/data01/hfftheo/$(whoami)"
+    DATA2_DIR="/data02/hfftheo/$(whoami)"
+else
+    HOME_DIR=${HOME}
+    WORK_DIR="/lustre/nyx/lcsc/$(whoami)"
+fi
 
-HOME_DIR=${HOME}
-WORK_DIR="/home/hfftheo/$(whoami)"
-DATA1_DIR="/data01/hfftheo/$(whoami)"
-DATA2_DIR="/data02/hfftheo/$(whoami)"
-for ((j=0; j<${#JOBSUBFROM[@]}; j++)); do
-    JOBSUBFROM[$j]=${JOBSUBFROM[$j]/$HOME_DIR/HOME}
-    JOBSUBFROM[$j]=${JOBSUBFROM[$j]/$WORK_DIR/WORK}
-    JOBSUBFROM[$j]=${JOBSUBFROM[$j]/$DATA1_DIR/DATA01}
-    JOBSUBFROM[$j]=${JOBSUBFROM[$j]/$DATA2_DIR/DATA02}
-done
-unset -v 'HOME_DIR' 'WORK_DIR' 'DATA1_DIR' 'DATA2_DIR'
+for ((j=0; j<${#JOB_SUBMISSION_FOLDER[@]}; j++)); do
+    JOB_SUBMISSION_FOLDER[$j]=${JOB_SUBMISSION_FOLDER[$j]/$HOME_DIR/HOME}
+    JOB_SUBMISSION_FOLDER[$j]=${JOB_SUBMISSION_FOLDER[$j]/$WORK_DIR/SCRATCH}
+    JOB_SUBMISSION_FOLDER[$j]=${JOB_SUBMISSION_FOLDER[$j]/$DATA1_DIR/DATA01}
+    JOB_SUBMISSION_FOLDER[$j]=${JOB_SUBMISSION_FOLDER[$j]/$DATA2_DIR/DATA02}
+done && unset -v 'HOME_DIR' 'WORK_DIR' 'DATA1_DIR' 'DATA2_DIR'
 
 #Some counting for the table 
-LONGEST_NAME=${JOBNAME[0]}
-for NAME in ${JOBNAME[@]}; do
+LONGEST_NAME=${JOB_NAME[0]}
+for NAME in ${JOB_NAME[@]}; do
     if [ ${#NAME} -gt ${#LONGEST_NAME} ]; then
         LONGEST_NAME=$NAME
     fi
 done
 RUNNING_JOBS=0
 PENDING_JOBS=0
-TOTAL_JOBS=${#JOBID_ARRAY[@]}
-for ((j=0; j<${#JOBSTATUS[@]}; j++)); do
-    if [[ ${JOBSTATUS[$j]} == "RUNNING" ]]; then
+TOTAL_JOBS=${#JOB_ID[@]}
+for ((j=0; j<${#JOB_STATUS[@]}; j++)); do
+    if [[ ${JOB_STATUS[$j]} == "RUNNING" ]]; then
         RUNNING_JOBS=$(($RUNNING_JOBS + 1))
-    elif [[ ${JOBSTATUS[$j]} == "PENDING" ]]; then
+    elif [[ ${JOB_STATUS[$j]} == "PENDING" ]]; then
         PENDING_JOBS=$(($PENDING_JOBS + 1))
     fi
 done && unset -v 'j'
@@ -141,12 +165,11 @@ OTHER_JOBS=$(($TOTAL_JOBS-$RUNNING_JOBS-$PENDING_JOBS))
 
 #------------------------------------------------------------------------------------------------------------------------------------------------#
 # If node usage is required just execute this code and exit
-
 if [ $NODE_USAGE = "TRUE" ]; then
     declare -A USED_NODES
     #Counting
-    for NODE in ${JOBFIRSTNODE[@]}; do
-	USED_NODES[$NODE]="${USED_NODES[$NODE]}+"
+    for NODE in ${JOB_NODELIST[@]}; do
+	    USED_NODES[$NODE]="${USED_NODES[$NODE]}+"
     done
     #Printing
     printf "\n\e[1;36m"
@@ -155,7 +178,7 @@ if [ $NODE_USAGE = "TRUE" ]; then
 
     printf "\e[38;5;13m%-15s%-10s\n\e[0m" "NODE" "RUNNING_JOBS"
     for NODE in ${!USED_NODES[@]}; do
-	printf "\e[38;5;39m%-15s\e[38;5;10m%-10s\e[38;5;49m%d\e[0m\n" "${NODE}" "${USED_NODES[$NODE]}" "$(grep -o '+' <<< "${USED_NODES[$NODE]}" | wc -l)"
+	    printf "\e[38;5;39m%-15s\e[38;5;10m%-10s\e[38;5;49m%d\e[0m\n" "${NODE}" "${USED_NODES[$NODE]}" "$(grep -o '+' <<< "${USED_NODES[$NODE]}" | wc -l)"
     done | sort -h
     
     printf "\n\e[38;5;202m  Total number of submitted jobs: $TOTAL_JOBS"
@@ -170,25 +193,24 @@ fi
 
 #------------------------------------------------------------------------------------------------------------------------------------------------#
 # If group betas is required just execute this code and exit
-
 if [ $GROUP_BETAS = "TRUE" ]; then
     declare -A JOB_PARAMETERS
     declare -A SEEDS_STATUS
     #Counting
-    for INDEX in ${!JOBNAME[@]}; do
-	NAME=${JOBNAME[$INDEX]}
-	JOB_PARAMETERS[${NAME%%_s*}]="${JOB_PARAMETERS[${NAME%%_s*}]} ${NAME##*_s}"
-	if [ ${JOBSTATUS[$INDEX]} = "RUNNING" ]; then
-	    SEEDS_STATUS[${NAME%%_s*}]="${SEEDS_STATUS[${NAME%%_s*}]} \e[38;5;10m${JOBSTATUS[$INDEX]:0:1}\e[0m"
-	elif [ ${JOBSTATUS[$INDEX]} = "PENDING" ]; then
-	    if [ ${JOBSTARTTIME[$i]} != "Unknown" ]; then
-		    SEEDS_STATUS[${NAME%%_s*}]="${SEEDS_STATUS[${NAME%%_s*}]} \e[38;5;11m${JOBSTATUS[$INDEX]:0:1}\e[0m"
-	    else
-		    SEEDS_STATUS[${NAME%%_s*}]="${SEEDS_STATUS[${NAME%%_s*}]} \e[38;5;9m${JOBSTATUS[$INDEX]:0:1}\e[0m"
+    for INDEX in ${!JOB_NAME[@]}; do
+	    NAME=${JOB_NAME[$INDEX]}
+	    JOB_PARAMETERS[${NAME%%_s*}]="${JOB_PARAMETERS[${NAME%%_s*}]} ${NAME##*_s}"
+	    if [ ${JOB_STATUS[$INDEX]} = "RUNNING" ]; then
+	        SEEDS_STATUS[${NAME%%_s*}]="${SEEDS_STATUS[${NAME%%_s*}]} \e[38;5;10m${JOB_STATUS[$INDEX]:0:1}\e[0m"
+	    elif [ ${JOB_STATUS[$INDEX]} = "PENDING" ]; then
+	        if [ ${JOB_START_TIME[$i]} != "Unknown" ]; then
+		        SEEDS_STATUS[${NAME%%_s*}]="${SEEDS_STATUS[${NAME%%_s*}]} \e[38;5;11m${JOB_STATUS[$INDEX]:0:1}\e[0m"
+	        else
+		        SEEDS_STATUS[${NAME%%_s*}]="${SEEDS_STATUS[${NAME%%_s*}]} \e[38;5;9m${JOB_STATUS[$INDEX]:0:1}\e[0m"
+	        fi
+        else
+            SEEDS_STATUS[${NAME%%_s*}]="${SEEDS_STATUS[${NAME%%_s*}]} \e[38;5;13m${JOB_STATUS[$INDEX]:0:1}\e[0m"
 	    fi
-    else
-        SEEDS_STATUS[${NAME%%_s*}]="${SEEDS_STATUS[${NAME%%_s*}]} \e[38;5;13m${JOBSTATUS[$INDEX]:0:1}\e[0m"
-	fi
     done
     #Printing
     printf "\n\e[1;36m"
@@ -197,7 +219,7 @@ if [ $GROUP_BETAS = "TRUE" ]; then
 
     printf "\e[38;5;4m%-40s%-30s%s\n\e[0m" "JOB_PARAMETERS" "QUEUED_SEEDS" "STATUS"
     for NAME in ${!JOB_PARAMETERS[@]}; do
-	printf "\e[38;5;14m%-40s\e[38;5;13m%-30s${SEEDS_STATUS[$NAME]:1}\e[0m\n" "${NAME}" "${JOB_PARAMETERS[$NAME]:1}"
+	    printf "\e[38;5;14m%-40s\e[38;5;13m%-30s${SEEDS_STATUS[$NAME]:1}\e[0m\n" "${NAME}" "${JOB_PARAMETERS[$NAME]:1}"
     done | sort -h
     
     printf "\n\e[38;5;202m  Total number of submitted jobs: $TOTAL_JOBS"
@@ -209,25 +231,25 @@ if [ $GROUP_BETAS = "TRUE" ]; then
 fi
 
 #------------------------------------------------------------------------------------------------------------------------------------------------#
-
-function PrintJobInformation(){
-    
+#Stabdard display
+if [ $DISPLAY_STANDARD_LIST = "TRUE" ]; then
+        
     #Table header
     COLUMNS_OF_THE_SHELL=$(tput cols)
     TABLE_FORMAT="%-8s%-5s%-$((2+${#LONGEST_NAME}))s%-5s%-25s%-5s%-19s%-5s%+14s%-5s%-s"
     printf "\n\e[1;36m"
     for (( c=1; c<=$(($COLUMNS_OF_THE_SHELL-3)); c++ )); do printf "="; done && unset -v 'c'
     printf "\e[0m\n"
-    printf "\e[38;5;202m$TABLE_FORMAT\e[0m\n"   "JOBID:" ""   "  JOB NAME:" ""   "STATUS:" ""   "START/END TIME:" ""   "WALL/RUNTIME:" ""   "SUBMITTED FROM:"
+    printf "\e[38;5;202m$TABLE_FORMAT\e[0m\n"   "JOB_ID:" ""   "  JOB NAME:" ""   "STATUS:" ""   "START/END TIME:" ""   "WALL/RUNTIME:" ""   "SUBMITTED FROM:"
 
     #Print table sorting according jobname
-    while [ ${#JOBNAME[@]} -gt 0 ]; do
-        i=$(FindPositionOfFirstMinimumOfArray "${JOBNAME[@]}")
+    while [ ${#JOB_NAME[@]} -gt 0 ]; do
+        i=$(FindPositionOfFirstMinimumOfArray "${JOB_NAME[@]}")
 
-        if [[ ${JOBSTATUS[$i]} == "RUNNING" ]]; then
+        if [[ ${JOB_STATUS[$i]} == "RUNNING" ]]; then
             printf "\e[0;32m"
-        elif [[ ${JOBSTATUS[$i]} == "PENDING" ]]; then
-            if [[ ${JOBSTARTTIME[$i]} != "Unknown" ]]; then
+        elif [[ ${JOB_STATUS[$i]} == "PENDING" ]]; then
+            if [[ ${JOB_START_TIME[$i]} != "Unknown" ]]; then
                 printf "\e[0;33m"
             else
                 printf "\e[0;31m"
@@ -236,33 +258,33 @@ function PrintJobInformation(){
             printf "\e[0;35m"
         fi
 
-        if [[ ${JOBSTATUS[$i]} == "RUNNING" ]]; then
-            printf "$TABLE_FORMAT\e[0m\n"   "${JOBID_ARRAY[$i]}" ""\
-                   "  ${JOBNAME[$i]}" ""\
-                   "${JOBSTATUS[$i]} on ${JOBFIRSTNODE[$i]}" ""\
-                   "${JOBENDTIME[$i]}" ""\
-                   "${JOBRUNTIME[$i]}" ""\
-                   "${JOBSUBFROM[$i]}"
+        if [[ ${JOB_STATUS[$i]} == "RUNNING" ]]; then
+            printf "$TABLE_FORMAT\e[0m\n"   "${JOB_ID[$i]}" ""\
+                   "  ${JOB_NAME[$i]}" ""\
+                   "${JOB_STATUS[$i]} on ${JOB_NODELIST[$i]}" ""\
+                   "${JOB_END_TIME[$i]}" ""\
+                   "${JOB_RUNTIME[$i]}" ""\
+                   "${JOB_SUBMISSION_FOLDER[$i]}"
         else
-            printf "$TABLE_FORMAT\e[0m\n"   "${JOBID_ARRAY[$i]}" ""\
-                   "  ${JOBNAME[$i]}" ""\
-                   "${JOBSTATUS[$i]}" ""\
-                   "${JOBSTARTTIME[$i]}" ""\
-                   "${JOBWALLTIME[$i]}" ""\
-                   "${JOBSUBFROM[$i]} on ${JOBSUBTIME[$i]}"
+            printf "$TABLE_FORMAT\e[0m\n"   "${JOB_ID[$i]}" ""\
+                   "  ${JOB_NAME[$i]}" ""\
+                   "${JOB_STATUS[$i]}" ""\
+                   "${JOB_START_TIME[$i]}" ""\
+                   "${JOB_WALLTIME[$i]}" ""\
+                   "${JOB_SUBMISSION_FOLDER[$i]} on ${JOB_SUBMISSION_TIME[$i]}"
         fi
 
-        unset JOBID_ARRAY[$i]; JOBID_ARRAY=( "${JOBID_ARRAY[@]}" )
-        unset JOBNAME[$i]; JOBNAME=( "${JOBNAME[@]}" )
-        unset JOBSTATUS[$i]; JOBSTATUS=( "${JOBSTATUS[@]}" )
-        unset JOBSTARTTIME[$i]; JOBSTARTTIME=( "${JOBSTARTTIME[@]}" )
-        unset JOBENDTIME[$i]; JOBENDTIME=( "${JOBENDTIME[@]}" )
-        unset JOBSUBTIME[$i]; JOBSUBTIME=( "${JOBSUBTIME[@]}" )
-        unset JOBSUBFROM[$i]; JOBSUBFROM=( "${JOBSUBFROM[@]}" )
-        unset JOBNUMNODES[$i]; JOBNUMNODES=( "${JOBNUMNODES[@]}" )
-        unset JOBFIRSTNODE[$i]; JOBFIRSTNODE=( "${JOBFIRSTNODE[@]}" )
-        unset JOBWALLTIME[$i]; JOBWALLTIME=( "${JOBWALLTIME[@]}" )
-        unset JOBRUNTIME[$i]; JOBRUNTIME=( "${JOBRUNTIME[@]}" )
+        unset JOB_ID[$i]; JOB_ID=( "${JOB_ID[@]}" )
+        unset JOB_NAME[$i]; JOB_NAME=( "${JOB_NAME[@]}" )
+        unset JOB_STATUS[$i]; JOB_STATUS=( "${JOB_STATUS[@]}" )
+        unset JOB_START_TIME[$i]; JOB_START_TIME=( "${JOB_START_TIME[@]}" )
+        unset JOB_END_TIME[$i]; JOB_END_TIME=( "${JOB_END_TIME[@]}" )
+        unset JOB_SUBMISSION_TIME[$i]; JOB_SUBMISSION_TIME=( "${JOB_SUBMISSION_TIME[@]}" )
+        unset JOB_SUBMISSION_FOLDER[$i]; JOB_SUBMISSION_FOLDER=( "${JOB_SUBMISSION_FOLDER[@]}" )
+        unset JOB_NUM_NODES[$i]; JOB_NUM_NODES=( "${JOB_NUM_NODES[@]}" )
+        unset JOB_NODELIST[$i]; JOB_NODELIST=( "${JOB_NODELIST[@]}" )
+        unset JOB_WALLTIME[$i]; JOB_WALLTIME=( "${JOB_WALLTIME[@]}" )
+        unset JOB_RUNTIME[$i]; JOB_RUNTIME=( "${JOB_RUNTIME[@]}" )
 
     done
 
@@ -272,12 +294,5 @@ function PrintJobInformation(){
     printf "\e[1;36m"
     for (( c=1; c<=$(($COLUMNS_OF_THE_SHELL-3)); c++ )); do printf "="; done && unset -v 'c'
     printf "\e[0m\n\n"
-}
 
-if [ $LOCAL_JOBS = 'TRUE' ]; then
-    PrintJobInformation | awk -v pathToMatch="$PWD" 'NR<=3{print $0}NR==2{line=$0}NR>3{if($0 ~ pathToMatch){print $0}}END{print line; printf "\n"}'
-else
-    PrintJobInformation
 fi
-
-
