881af0200837af8c838fa55db219d94b38bf2fc1
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2016-09-28 18:04:21 +0200

    Worked over FittingUtilities

--- UtilityFunctions.sh
+++ UtilityFunctions.sh
@@ -28,12 +28,12 @@ function TimeStringToSecond(){
     local STRING_SEPARATED=( $(sed 's/\([smhd]\)/\1 /g' <<< "$1") )
     local TOTAL_TIME_IN_SECONDS=0
     for ELEMENT in ${STRING_SEPARATED[@]}; do
-	case $ELEMENT in
-	    *d) TOTAL_TIME_IN_SECONDS=$(( $TOTAL_TIME_IN_SECONDS + 86400*${ELEMENT%?} )) ;;
-	    *h) TOTAL_TIME_IN_SECONDS=$(( $TOTAL_TIME_IN_SECONDS +  3600*${ELEMENT%?} )) ;;
-	    *m) TOTAL_TIME_IN_SECONDS=$(( $TOTAL_TIME_IN_SECONDS +    60*${ELEMENT%?} )) ;;
-	    *s) TOTAL_TIME_IN_SECONDS=$(( $TOTAL_TIME_IN_SECONDS +       ${ELEMENT%?} )) ;;
-	esac
+	    case $ELEMENT in
+	        *d) TOTAL_TIME_IN_SECONDS=$(( $TOTAL_TIME_IN_SECONDS + 86400*${ELEMENT%?} )) ;;
+	        *h) TOTAL_TIME_IN_SECONDS=$(( $TOTAL_TIME_IN_SECONDS +  3600*${ELEMENT%?} )) ;;
+	        *m) TOTAL_TIME_IN_SECONDS=$(( $TOTAL_TIME_IN_SECONDS +    60*${ELEMENT%?} )) ;;
+	        *s) TOTAL_TIME_IN_SECONDS=$(( $TOTAL_TIME_IN_SECONDS +       ${ELEMENT%?} )) ;;
+	    esac
     done && unset -v 'ELEMENT' 
     echo "$TOTAL_TIME_IN_SECONDS"
 }
@@ -50,12 +50,53 @@ function SecondsToTimeStringWithDays(){
 function MinimumOfArray(){
     local MIN=$1; shift
     while [ "$1" != "" ]; do
-	if [ $(echo "$1 $MIN" | awk '{if($1<$2){print 1}else{print 0}}') -eq 1 ]; then
-	    MIN=$1
-	fi
-	shift
+	    if [ $(echo "$1 $MIN" | awk '{print ($1<$2)}') -eq 1 ]; then
+	        MIN=$1
+	    fi
+	    shift
     done
     echo "$MIN"
+}
+
+function KeyOfMinimumOfArray(){
+    local COUNTER=0
+    local KEY_AT_MIN=0
+    local MIN=$1; shift
+    while [ "$1" != "" ]; do
+        (( COUNTER++ ))
+	    if [ $(echo "$1 $MIN" | awk '{print ($1<$2)}') -eq 1 ]; then
+	        MIN=$1
+            KEY_AT_MIN=$COUNTER
+	    fi
+	    shift
+    done
+    echo "$KEY_AT_MIN"
+}
+
+function MaximumOfArray(){
+    local MAX=$1; shift
+    while [ "$1" != "" ]; do
+	    if [ $(echo "$1 $MAX" | awk '{print ($1>$2)}') -eq 1 ]; then
+	        MAX=$1
+	    fi
+	    shift
+    done
+    echo "$MAX"
+}
+
+function KeyOfMaximumOfArray(){
+    local COUNTER=0
+    local KEY_AT_MAX=0
+    local MAX=$1; shift
+    while [ "$1" != "" ]; do
+        (( COUNTER++ ))
+	    if [ $(echo "$1 $MAX" | awk '{print ($1>$2)}') -eq 1 ]; then
+	        MAX=$1
+            KEY_AT_MAX=$COUNTER
+	    fi
+	    shift
+    done
+    echo "$KEY_AT_MAX"
 }
 
 function FindPositionOfFirstMinimumOfArray(){
@@ -63,20 +104,20 @@ function FindPositionOfFirstMinimumOfArray(){
     local ARRAY=("$@")
     local MIN=$(MinimumOfArray "${ARRAY_TMP[@]}")
     for (( i=0; i<${#ARRAY[@]}; i++ )); do
-	if [ "${ARRAY[$i]}" = "${MIN}" ]; then
-	    echo $i;
-	    break
-	fi
+	    if [ "${ARRAY[$i]}" = "${MIN}" ]; then
+	        echo $i;
+	        break
+	    fi
     done
 }
 
 function LengthOfLongestEntryInArray(){
     local LENGTH_MAX=${#1}; shift
     while [ "$1" != "" ]; do
-	if [ ${#1} -gt $LENGTH_MAX ]; then
-	    LENGTH_MAX=${#1}
-	fi
-	shift
+	    if [ ${#1} -gt $LENGTH_MAX ]; then
+	        LENGTH_MAX=${#1}
+	    fi
+	    shift
     done
     echo "$LENGTH_MAX"
 }
@@ -98,9 +139,9 @@ function KeyInArray() {
     local ARRAY=$2
     local KEY=$1
     if eval '[ ${'$ARRAY'[$KEY]+isSet} ]'; then
-	return 0;
+	    return 0;
     else
-	return 1;
+	    return 1;
     fi
 }
 
@@ -119,7 +160,7 @@ function PrintArray(){
     local INDEX=""
     [ $(eval echo "\${#$NAME_OF_THE_ARRAY[@]}") -eq 0 ] && echo "Array $NAME_OF_THE_ARRAY is empty!" && return
     for INDEX in $(eval echo "\${!$NAME_OF_THE_ARRAY[@]}"); do
-	echo "$NAME_OF_THE_ARRAY[$INDEX]=$(eval echo "\${$NAME_OF_THE_ARRAY[$INDEX]}")"
+	    echo "$NAME_OF_THE_ARRAY[$INDEX]=$(eval echo "\${$NAME_OF_THE_ARRAY[$INDEX]}")"
     done
 }
 
