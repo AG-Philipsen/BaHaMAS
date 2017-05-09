5c7018acc975357994a9dc29670483160f4b59e7
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2017-05-09 10:38:28 +0200

    Improved helper documentation with consistent syntax

--- CommandLineParser_aux.bash
+++ CommandLineParser_aux.bash
@@ -69,8 +69,7 @@ function PrintHelper(){
     __static__AddOptionToHelper "-h | --help"                      ""
     __static__AddOptionToHelper "--jobscript_prefix"               "default value = $JOBSCRIPT_PREFIX"
     __static__AddOptionToHelper "--chempot_prefix"                 "default value = $CHEMPOT_PREFIX"
-    __static__AddOptionToHelper "--kappa_prefix"                   "$(cecho "default value = k    " B "(Wilson    Case ONLY)" uB)"
-    __static__AddOptionToHelper "--mass_prefix"                    "$(cecho "default value = mass " B "(Staggered Case ONLY)" uB)"
+    __static__AddOptionToHelper "--mass_prefix"                    "default value = $MASS_PREFIX"
     __static__AddOptionToHelper "--ntime_prefix"                   "default value = $NTIME_PREFIX"
     __static__AddOptionToHelper "--nspace_prefix"                  "default value = $NSPACE_PREFIX"
     __static__AddOptionToHelper "--beta_prefix"                    "default value = $BETA_PREFIX"
@@ -93,38 +92,41 @@ function PrintHelper(){
     __static__AddOptionToHelper -e "--submitonly"                 "jobs will be submitted (no files are created)"
     __static__AddOptionToHelper -e "-t | --thermalize"            "The thermalization is done."
     __static__AddOptionToHelper -e "-c     | --continue"          "Unfinished jobs will be continued doing the nr. of measurements specified in the input"
-    __static__AddOptionToHelper -e "-c=[#] | --continue=[#]"      "file .If a number is specified, jobs will be continued up to the specified number."\
+    __static__AddOptionToHelper -e "-c[=#] | --continue[=#]"      "file. If a number is specified, jobs will be continued up to the specified number."\
                                 "$(cecho "To resume a simulation from a given trajectory, add " bc "resumefrom=[number]" $helperColor " in")"\
                                 "$(cecho "the betasfile. Use " bc "resumefrom=last" $helperColor " in the betasfile to resume a simulation")"\
                                 "$(cecho "from the last saved " p "conf.[[:digit:]]+" $helperColor " file.")"
     __static__AddOptionToHelper -e "-C     | --continueThermalization"      "Unfinished thermalizations will be continued doing the nr. of measurements specified in the"
-    __static__AddOptionToHelper -e "-C=[#] | --continueThermalization=[#]"  "input file. If a number is specified, thermalizations will be continued up to the specified"\
+    __static__AddOptionToHelper -e "-C[=#] | --continueThermalization[=#]"  "input file. If a number is specified, thermalizations will be continued up to the specified"\
                                 "$(cecho "number. To resume a thermalization from a given trajectory, add " bc "resumefrom=[number]" $helperColor " in")"\
                                 "$(cecho "the betasfile. Use " bc "resumefrom=last" $helperColor " in the betasfile to resume a thermalization")"\
                                 "$(cecho "from the last saved " p "conf.[[:digit:]]+" $helperColor " file.")"
     __static__AddOptionToHelper -e "-l | --liststatus" "A report of the local simulation status for all beta will be displayed"\
-                                "$(cecho  U "Secondary options" uU ": " $mutuallyExclusiveColor "--measureTime" $helperColor " to get information about the trajectory time")"\
+                                "$(cecho  B "Secondary options" uB ": " $mutuallyExclusiveColor "--measureTime" $helperColor " to get information about the trajectory time")"\
                                 "$(cecho "                   " $mutuallyExclusiveColor "--showOnlyQueued" $helperColor " not to show status about not queued jobs")"
-    __static__AddOptionToHelper -e "--accRateReport=[#]" "The acceptance rates will be computed on the output files of the given"\
-                                "betas every [#] configurations and summarized in a table."
+    __static__AddOptionToHelper -e "--accRateReport[=number]" "The acceptance rates will be computed on the output files of the given beta every "\
+                                "1000 trajectories and summarized in a table. If a number is specified, this is used"\
+                                "as interval width. Only the acceptance for complete intervals is calculated."
     __static__AddOptionToHelper -e "--cleanOutputFiles" "The output files referred to the betas contained in the betas file are cleaned"\
                                 "(repeated lines are eliminated). For safety reason, a backup of the output file is done"\
                                 "(it is left in the output file folder with the name outputfilename_date)."\
                                 "$(cecho "Secondary options: " $mutuallyExclusiveColor "--all" $helperColor " to clean output files for all betas in present folder")"
-    __static__AddOptionToHelper -e "--completeBetasFile[=number]" "The beta file is completed adding for each beta new chains in order to have as many chain as specified."\
-                                "$(cecho "If no number is specified, 4 is used. This option, if " $mutuallyExclusiveColor "--doNotUseMultipleChains" $helperColor " has not been given, uses")"\
-                                "the seed in the second field to generate new chains (or one new field containing the seed is inserted)."
+    __static__AddOptionToHelper -e "--completeBetasFile[=number]" "The beta file is completed adding for each beta new chains in order to have as many"\
+                                "chain as specified. If no number is specified, 4 is used. This option, if"\
+                                "$(cecho $mutuallyExclusiveColor "--doNotUseMultipleChains" $helperColor " has not been given, uses the seed in the second field to")"\
+                                "generate new chains (or one new field containing the seed is inserted)."
     __static__AddOptionToHelper -e "-U | --uncommentBetas" "This option uncomments the specified betas (all remaining entries will be commented)."\
                                 "The betas can be specified either with a seed or without. The format of the specified string"\
                                 "$(cecho "can either contain the output of the " $mutuallyExclusiveColor "--liststatus" $helperColor " option, e.g. " c "5.4380_s5491_NC" $helperColor ", or simply")"\
                                 "$(cecho "beta values like " c "5.4380" $helperColor ", or a mix of both. If pure beta values are given then all seeds")"\
                                 "of the given beta value will be uncommented."
-    __static__AddOptionToHelper -e "-u | --commentBetas" "$(cecho "Is the reverse option of the " $mutuallyExclusiveColor "--uncommentBetas" $helperColor " option")"
+    __static__AddOptionToHelper -e "-u | --commentBetas" "$(cecho "The reverse option of " $mutuallyExclusiveColor "--uncommentBetas" $helperColor ".")"
     __static__AddOptionToHelper -e "-i | --invertConfigurations" "Invert configurations and produce correlator files for betas and seed specified in the betas file."
-    __static__AddOptionToHelper -e "-d | --dataBase" "Update, display and filter database. This is a subprogram plenty of functionalities. Run this script with"\
-                                "$(cecho "the option " $mutuallyExclusiveColor "--helpDatabase" $helperColor " to get an explanation about the various possibilities. To work with the database,")"\
-                                "$(cecho "specify the " $mutuallyExclusiveColor "--dataBase" $helperColor " option followed by all the database options. Differently said, all options given")"\
-                                "$(cecho "after " $mutuallyExclusiveColor "--dataBase" $helperColor " are options for the database subprogram.")"
+    __static__AddOptionToHelper -e "-d | --dataBase" "Update, display and filter database. This is a subprogram plenty of functionality."\
+                                "$(cecho "Run this script with the option " $mutuallyExclusiveColor "--helpDatabase" $helperColor " to get an explanation about the various")"\
+                                "$(cecho "possibilities. To work with the database, specify the " $mutuallyExclusiveColor "--dataBase" $helperColor " option followed by all")"\
+                                "$(cecho "the database options. Differently said, all options given after " $mutuallyExclusiveColor "--dataBase" $helperColor " are options")"\
+                                "for the database subprogram."
     cecho ''
     cecho ly B\
           'NOTE:' uB " The " $mutuallyExclusiveColor "blue options" ly ' are mutually exclusive and they are all ' c 'FALSE' ly ' by default! In other words, if none of them\n'\
