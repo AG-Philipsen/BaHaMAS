64d4028087c72bfc8dae5c01ba0ea9783f41a6e8
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2017-05-12 16:15:27 +0200

    Used cecho function in path functions

--- PathManagementFunctionality.bash
+++ PathManagementFunctionality.bash
@@ -1,7 +1,7 @@
 function CheckWilsonStaggeredVariables()
 {
     if [ "$WILSON" == "$STAGGERED" ]; then
-        printf "\n\e[0;31m Variables WILSON and STAGGERED both set to the same value (please check the position from where the script was run)! Aborting...\n\n\e[0m"
+        cecho lr "\n The variables " emph "WILSON" " and " emph "STAGGERED" " are both set to the same value (please check the position from where the script was run)! Aborting...\n"
         exit -1
     fi
 }
@@ -10,7 +10,10 @@ function CheckWilsonStaggeredVariables()
 function IsAnyParameterUnsetAmong()
 {
     for PREFIX in $@; do
-        [[ -z ${PARAMETER_VARIABLE_NAMES[$PREFIX]:+x} ]] &&  printf "\n\e[0;31m Accessing PARAMETER_VARIABLE_NAMES array with not existing prefix! Aborting...\n\n\e[0m" && exit -1
+        if [[ -z ${PARAMETER_VARIABLE_NAMES[$PREFIX]:+x} ]]; then
+            cecho lr "\n Accessing " emph "PARAMETER_VARIABLE_NAMES" " array with not existing prefix in funciton " emph "$FUNCNAME" "! Aborting...\n"
+            exit -1
+        fi
         if [ "${!PARAMETER_VARIABLE_NAMES[$PREFIX]}" = "" ]; then
             return 0
         fi
@@ -21,7 +24,10 @@ function IsAnyParameterUnsetAmong()
 #This function get the prefixes of parameters with which the string has to be built (order as given) -> e.g. Nf2_mui0_ns8
 function GetParametersString()
 {
-    IsAnyParameterUnsetAmong $@ &&  printf "\n\e[0;31m Function \"$FUNCNAME\" called before extracting parameters! Aborting...\n\n\e[0m" && exit -1
+    if IsAnyParameterUnsetAmong $@; then
+        cecho lr "\n Function " emph "$FUNCNAME" " called before extracting parameters! Aborting...\n"
+        exit -1
+    fi
     local RESULTING_STRING=""
     for PREFIX in $@; do
         RESULTING_STRING="$RESULTING_STRING${PREFIX}${!PARAMETER_VARIABLE_NAMES[$PREFIX]}_"
@@ -32,7 +38,10 @@ function GetParametersString()
 #This function get the prefixes of parameters with which the path has to be built (order as given) -> e,g, /Nf2/nt4/ns8
 function GetParametersPath()
 {
-    IsAnyParameterUnsetAmong $@ &&  printf "\n\e[0;31m Function \"$FUNCNAME\" called before extracting parameters! Aborting...\n\n\e[0m" && exit -1
+    if IsAnyParameterUnsetAmong $@; then
+        cecho lr "\n Function " emph "$FUNCNAME" " called before extracting parameters! Aborting...\n"
+        exit -1
+    fi
     local RESULTING_PATH=""
     for PREFIX in $@; do
         RESULTING_PATH="$RESULTING_PATH/${PREFIX}${!PARAMETER_VARIABLE_NAMES[$PREFIX]}"
@@ -43,9 +52,11 @@ function GetParametersPath()
 #This function set the global variables PARAMETERS_PATH and PARAMETERS_STRING after having checked that the parameters have been extracted
 function SetParametersPathAndString()
 {
-
     #TODO: Use function GetParametersString giving ${PARAMETER_PREFIXES[@]} to it and build PARAMETERS_PATH as "/${PARAMETERS_STRING[@]//_/\/}"
-    IsAnyParameterUnsetAmong $@ &&  printf "\n\e[0;31m Function \"$FUNCNAME\" called before extracting parameters! Aborting...\n\n\e[0m" && exit -1
+    if IsAnyParameterUnsetAmong $@; then
+        cecho lr "\n Function " emph "$FUNCNAME" " called before extracting parameters! Aborting...\n"
+        exit -1
+    fi
     local PARAMETERS_VALUE=([$NFLAVOUR_POSITION]=$NFLAVOUR [$CHEMPOT_POSITION]=$CHEMPOT [$MASS_POSITION]=$MASS [$NTIME_POSITION]=$NTIME [$NSPACE_POSITION]=$NSPACE)
     for ((i=0; i<${#PARAMETER_PREFIXES[@]}; i++)); do
         PARAMETERS_PATH="$PARAMETERS_PATH/${PARAMETER_PREFIXES[$i]}${PARAMETERS_VALUE[$i]}"
@@ -58,12 +69,18 @@ function SetParametersPathAndString()
 #NOTE: The prefix must appear after a slash in the path!
 function ReadSingleParameterFromPath()
 {
-    [ $# -ne 2 ] &&  printf "\n\e[0;31m Function \"$FUNCNAME\" called with wrong number of parameters! Aborting...\n\n\e[0m" && exit -1
-    [ -z ${PARAMETER_VARIABLE_NAMES[$2]:+x} ] &&  printf "\n\e[0;31m Function \"$FUNCNAME\" called with unknown prefix! Aborting...\n\n\e[0m" && exit -1
+    if [ $# -ne 2 ]; then
+        cecho lr "\n Function " emph "$FUNCNAME" " called with wrong number of parameters! Aborting...\n"
+        exit -1
+    fi
+    if [ -z ${PARAMETER_VARIABLE_NAMES[$2]:+x} ]; then
+        cecho lr  "\n Function " emph "$FUNCNAME" " called with unknown prefix! Aborting...\n"
+        exit -1
+    fi
     local PATH_TO_SEARCH_IN="/$1/" #Add in front and back a "/" just to be general in the search
     local PREFIX_TO_BE_USED="$2"
     if [ $(grep -oc "/$PREFIX_TO_BE_USED" <<< "$PATH_TO_SEARCH_IN") -ne 1 ]; then
-        printf "\n\e[0;31m Unable to recover \"$PREFIX_TO_BE_USED\" from the path \"$1\". Aborting...\n\n\e[0m"
+        cecho lr "\n Unable to recover " emph "$PREFIX_TO_BE_USED" " from the path " dir "$1" ". Aborting...\n"
         exit -1
     fi
     local PIECE_OF_PATH_WITH_PARAMETER="$(grep -o "/${PREFIX_TO_BE_USED}[^/]*" <<< "$PATH_TO_SEARCH_IN")"
@@ -75,12 +92,18 @@ function ReadSingleParameterFromPath()
 # At the moment the global variable is set to a readonly array value.
 function ReadSingleParameterFromPathWithMultipleOccurence()
 {
-    [ $# -ne 2 ] &&  printf "\n\e[0;31m Function \"$FUNCNAME\" called with wrong number of parameters! Aborting...\n\n\e[0m" && exit -1
-    [ -z ${PARAMETER_VARIABLE_NAMES[$2]:+x} ] &&  printf "\n\e[0;31m Function \"$FUNCNAME\" called with unknown prefix! Aborting...\n\n\e[0m" && exit -1
+    if [ $# -ne 2 ]; then
+        cecho lr "\n Function " emph "$FUNCNAME" " called with wrong number of parameters! Aborting...\n"
+        exit -1
+    fi
+    if [ -z ${PARAMETER_VARIABLE_NAMES[$2]:+x} ]; then
+        cecho lr "\n Function " emph "$FUNCNAME" " called with unknown prefix! Aborting...\n"
+        exit -1
+    fi
     local PATH_TO_SEARCH_IN="$1"
     local PREFIX_TO_BE_USED="$2"
     if [ $(grep -oc "$PREFIX_TO_BE_USED" <<< "$PATH_TO_SEARCH_IN") -lt 1 ]; then
-        printf "\n\e[0;31m \"$PREFIX_TO_BE_USED\" not found in the path \"$PATH_TO_SEARCH_IN\". Aborting...\n\n\e[0m"
+        cecho lr "\n Prefix " emph "$PREFIX_TO_BE_USED" " not found in the path " dir "$PATH_TO_SEARCH_IN" ". Aborting...\n"
         exit -1
     fi
     #TODO: At the moment this works only for numbers. The parameter regex should be used here but it implies to have access to it through the prefix!
@@ -88,44 +111,49 @@ function ReadSingleParameterFromPathWithMultipleOccurence()
 }
 
 
-#This function get a bunch of prefixes and checks that the corresponding variable has a value that makes sense
+# This function get a bunch of prefixes and checks that the corresponding variable has a value that makes sense
+# NOTE: Here we check any variable content as if it could contain several values. This should not hurt, since
+#       ${var[@]} should coincide with $var in case of a simple variable.
 function CheckParametersExtractedFromPath()
 {
-    IsAnyParameterUnsetAmong $@ &&  printf "\n\e[0;31m Function \"$FUNCNAME\" called before extracting parameters! Aborting...\n\n\e[0m" && exit -1
+    if IsAnyParameterUnsetAmong $@; then
+        cecho lr "\n Function" emph "$FUNCNAME" " called before extracting parameters! Aborting...\n"
+        exit -1
+    fi
     for PREFIX in $@; do
         case "${PARAMETER_VARIABLE_NAMES[$PREFIX]}" in
             MASS)
                 for VALUE in ${MASS[@]}; do
                     if [[ ! $VALUE =~ ^${MASS_REGEX//\\/}$ ]]; then
-                        printf "\n\e[0;31m Parameter \"$MASS_PREFIX\" extracted from the path not allowed! Aborting...\n\n\e[0m"; exit -1
+                        cecho lr "\n Parameter " emph "$MASS_PREFIX" " extracted from the path not allowed! Aborting...\n"; exit -1
                     fi
                 done && unset -v 'VALUE' ;;
             NTIME)
                 for VALUE in ${NTIME[@]}; do
                     if [[ ! $VALUE =~ ^${NTIME_REGEX//\\/}$ ]]; then
-                        printf "\n\e[0;31m Parameter \"$NTIME_PREFIX\" extracted from the path not allowed! Aborting...\n\n\e[0m"; exit -1
+                        cecho lr "\n Parameter " emph "$NTIME_PREFIX" " extracted from the path not allowed! Aborting...\n"; exit -1
                     fi
                 done && unset -v 'VALUE' ;;
             NSPACE)
                 for VALUE in ${NSPACE[@]}; do
                     if [[ ! $VALUE =~ ^${NSPACE_REGEX//\\/}$ ]]; then
-                        printf "\n\e[0;31m Parameter \"$NSPACE_PREFIX\" extracted from the path not allowed! Aborting...\n\n\e[0m"; exit -1
+                        cecho lr "\n Parameter " emph "$NSPACE_PREFIX" " extracted from the path not allowed! Aborting...\n"; exit -1
                     fi
                 done && unset -v 'VALUE' ;;
             NFLAVOUR)
                 for VALUE in ${NFLAVOUR[@]}; do
                     if [[ ! $VALUE =~ ^${NFLAVOUR_REGEX//\\/}$ ]]; then
-                        printf "\n\e[0;31m Parameter \"$NFLAVOUR_PREFIX\" extracted from the path not allowed! Aborting...\n\n\e[0m"; exit -1
+                        cecho lr "\n Parameter " emph "$NFLAVOUR_PREFIX" " extracted from the path not allowed! Aborting...\n"; exit -1
                     fi
                 done && unset -v 'VALUE' ;;
             CHEMPOT)
                 for VALUE in ${CHEMPOT[@]}; do
                     if [[ ! $VALUE =~ ^${CHEMPOT_REGEX//\\/}$ ]]; then
-                        printf "\n\e[0;31m Parameter \"$CHEMPOT_PREFIX\" extracted from the path not allowed! Aborting...\n\n\e[0m"; exit -1
+                        cecho lr "\n Parameter " emph "$CHEMPOT_PREFIX" " extracted from the path not allowed! Aborting...\n"; exit -1
                     fi
                 done && unset -v 'VALUE' ;;
             *)
-                printf "\n\e[0;31m Unknown prefix given to function \"$FUNCNAME\"! Aborting...\n\n\e[0m" ; exit -1 ;;
+                cecho lr "\n Unknown prefix given to function " emph "$FUNCNAME" "! Aborting...\n" ; exit -1 ;;
         esac
     done && unset -v 'PREFIX'
 }
@@ -147,7 +175,7 @@ function ReadParametersFromPath()
     local PATH_TO_BE_USED="/$1/"
     for ((i=0; i<${#PARAMETER_PREFIXES[@]}; i++)); do
         if [ $(grep -o "/${PARAMETER_PREFIXES[$i]}" <<< "$PATH_TO_BE_USED" | wc -l) -ne 1 ]; then
-            printf "\n\e[0;31m Unable to recover \"${PARAMETER_PREFIXES[$i]}\" from the path \"$1\". Aborting...\n\n\e[0m"
+            cecho lr "\n Unable to recover " emph "${PARAMETER_PREFIXES[$i]}" " from the path " dir "$1" ". Aborting...\n"
             exit -1
         fi
         PARAMETERS_VALUE[$i]=$(awk -v expr="${PARAMETER_PREFIXES[$i]}" \
@@ -162,19 +190,19 @@ function ReadParametersFromPath()
 
     #Check that the recovered parameters make sense (remove escape characters from regex)
     if [[ ! $MASS =~ ^${MASS_REGEX//\\/}$ ]]; then
-        printf "\n\e[0;31m Parameter \"$MASS_PREFIX\" from the path \"$1\" not allowed! Aborting...\n\n\e[0m"
+        cecho lr "\n Parameter " emph "$MASS_PREFIX" " extracted from the path not allowed! Aborting...\n"; exit -1
         exit -1
     elif [[ ! $NTIME =~ ^${NTIME_REGEX//\\/}$ ]]; then
-        printf "\n\e[0;31m Parameter \"$NTIME_PREFIX\" from the path \"$1\" not allowed! Aborting...\n\n\e[0m"
+        cecho lr "\n Parameter " emph "$NTIME_PREFIX" " extracted from the path not allowed! Aborting...\n"; exit -1
         exit -1
     elif [[ ! $NSPACE =~ ^${NSPACE_REGEX//\\/}$ ]]; then
-        printf "\n\e[0;31m Parameter \"$NSPACE_PREFIX\" from the path \"$1\" not allowed! Aborting...\n\n\e[0m"
+        cecho lr "\n Parameter " emph "$NSPACE_PREFIX" " extracted from the path not allowed! Aborting...\n"; exit -1
         exit -1
     elif [[ ! $NFLAVOUR =~ ^${NFLAVOUR_REGEX//\\/}$ ]]; then
-        printf "\n\e[0;31m Parameter \"$NFLAVOUR_PREFIX\" from the path \"$1\" not allowed! Aborting...\n\n\e[0m"
+        cecho lr "\n Parameter " emph "$NFLAVOUR_PREFIX" " extracted from the path not allowed! Aborting...\n"; exit -1
         exit -1
     elif [[ ! $CHEMPOT =~ ^${CHEMPOT_REGEX//\\/}$ ]]; then
-        printf "\n\e[0;31m Parameter \"$CHEMPOT_PREFIX\" from the path \"$1\" not allowed! Aborting...\n\n\e[0m"
+        cecho lr "\n Parameter " emph "$CHEMPOT_PREFIX" " extracted from the path not allowed! Aborting...\n"; exit -1
         exit -1
     fi
     #Set parameters path
@@ -186,7 +214,7 @@ function CheckSingleOccurrenceInPath()
     for var in $@; do
         Var=$(grep -o "$var" <<< "$(pwd)" | wc -l)
         if [ $Var -ne 1 ] ; then
-            printf "\n\e[0;31m The string \"$var\" must occur once and only once in the path! Aborting...\n\n\e[0m"
+            cecho lr "\n The string " emph "$var" " must occur " B "once and only once" uB " in the path! Aborting...\n"
             exit 1
         fi
     done
