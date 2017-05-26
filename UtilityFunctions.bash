c9e5cb0404a4fd910e2734bd4d00967e34b8b1cb
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2017-05-26 18:00:59 +0200

    Fixed bug in printing associative arrays

--- UtilityFunctions.bash
+++ UtilityFunctions.bash
@@ -183,7 +183,7 @@ function PrintArray()
     elif [[ $ARRAY_DECLARATION =~ ^declare\ -A ]]; then # associative array
         eval "declare -A ARRAY=${ARRAY_DECLARATION#*=}"
         for INDEX in "${!ARRAY[@]}"; do
-            printf "%s%s%s\n" "$1[\"$INDEX\"]=${ARRAY[" "$INDEX" "]}"
+            printf "%s\n" "$1[\"$INDEX\"]=${ARRAY[$INDEX]}"
         done
         return 0
     else
@@ -191,7 +191,6 @@ function PrintArray()
         return 1
     fi
 }
-
 
 function ConvertFromBytesToHumanReadable()
 {
