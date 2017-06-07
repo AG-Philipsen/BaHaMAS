e6db0774f97f175a4ff0a70551edfde12b973b6f
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2017-06-07 11:51:36 +0200

    Improved setup procedure to reuse set variables

--- ClusterIndependentCode/Setup.bash
+++ ClusterIndependentCode/Setup.bash
@@ -56,38 +56,55 @@ function __static__ParseSetupResultAndFillInUserVariablesArray()
 
 function MakeInteractiveSetupAndCreateUserDefinedVariablesFile()
 {
-    BHMAS_coloredOutput='FALSE' #To use cecho
     local filenameUserSetup filenameTemplate\
           dialogDimensions dialogHeight dialogWidth formHeight\
           variableNameFieldLength variableValueFieldLength\
-          formHeader commandToBeExecuted variable\
-          index variableName variableValue variableNames
+          formHeader commandToBeExecuted variable occurences\
+          unableToRecover index variableName variableValue variableNames
     declare -A userVariables=()
     filenameUserSetup="$1"
     filenameTemplate="${filenameUserSetup/.bash/_template.bash}"
-    #Start setting up colored output
-    commandToBeExecuted="dialog --keep-tite --yesno 'Would you like to activate colored output?' 5 50"
-    __static__FireUpTheDialogBoxStoringResultAndActingAccordingly\
-        "__static__SetColoredOutput TRUE"\
-        "__static__SetColoredOutput FALSE"
     #Read variables from template file
     for variable in $(awk '/^($|[#]+)/{next}{print $0}' "$filenameTemplate" | grep -o "BHMAS_.*"); do
         variableName=${variable%%=*}
+        if [ $variableName = 'BHMAS_coloredOutput' ]; then #Treat it separately to use here cecho
+            continue
+        fi
         variableValue="$(sed "s/['\"]//g" <<< "${variable##*=}")"
         variableNames+=( $variableName ) #To be used later to keep names in order
         userVariables[$variableName]="$variableValue"
     done
     #Fill in variable names from maybe existent user setup
     if [ -f "$filenameUserSetup" ]; then
+        BHMAS_coloredOutput=( $(sed -n "s/^[^#].*BHMAS_coloredOutput=\(.*\)/\1/p" $filenameUserSetup | sed "s/['\"]//g") )
+        if [ ${#BHMAS_coloredOutput[@]} -ne 1 ]; then
+            BHMAS_coloredOutput='FALSE'
+        fi
+        unableToRecover=()
         for variable in ${!userVariables[@]}; do
             #TODO: What about several formulations?!
-            if [ $(awk '/^($|[#]+)/{next}{print $0}' "$filenameUserSetup" | grep -o "${variable}=.*" | wc -l) -eq 1 ]; then
-                userVariables[$variable]="$(sed -n "s/^[^#].*${variable}=\(.*\)/\1/p" $filenameUserSetup | sed "s/['\"]//g")"
+            occurences=( $(sed -n "s/^[^#].*\(${variable}=.*\)/\1/p" $filenameUserSetup | sed "s/['\"]//g") )
+            if [ ${#occurences[@]} -le 1 ]; then
+                userVariables[$variable]=${occurences[0]##*=}
             else
-                printf " WARNING: Unable to recover set value for variable \"$variable\"\n"
+                unableToRecover+=( $variable )
             fi
         done
+        if [ ${#unableToRecover[@]} -ne 0 ]; then
+            cecho B ly "\n " U "WARNING" uU ":" uB " Unable to recover the previously set value for the following variable(s):"
+            for variable in ${unableToRecover[@]}; do
+                cecho ly "           - " lo "$variable"
+            done
+        fi
     fi
+    #Start setting up colored output
+    commandToBeExecuted="dialog --keep-tite --colors
+                                --backtitle 'BaHaMAS setup'
+                                --title 'BaHaMAS output'
+                                --yesno '\nWould you like to activate \Zb\Z4co\Z5lo\Z1red\Zn output?' 6 60"
+    __static__FireUpTheDialogBoxStoringResultAndActingAccordingly\
+        "__static__SetColoredOutput TRUE"\
+        "__static__SetColoredOutput FALSE"
     #Continue with BaHaMAS user variables
     exec 3>&1; dialogDimensions=$(dialog --print-maxsize 2>&1 1>&3); exec 3>&-
     dialogHeight=$(grep -o "[0-9]\+" <<< "$dialogDimensions" | head -n1 | awk '{print int(0.6*$1)}')
@@ -95,10 +112,12 @@ function MakeInteractiveSetupAndCreateUserDefinedVariablesFile()
     formHeight=30 #Hard-coded since it seems there is an implicit maximum depending on the dialogHeight
     variableNameFieldLength=$(( $(LengthOfLongestEntryInArray ${variableNames[@]}) +8 ))
     variableValueFieldLength=200 #It should be enough
-    formHeader='\nPlease, provide the following information (the content of a previous filled out variable can be accessed with the ${nameOfTheVariable} syntax):'
-    commandToBeExecuted="dialog --keep-tite --clear --ok-label 'Configure'
-                                --exit-label 'Abort'
+    formHeader='\n\ZbPlease, provide the following information (the content of a previous filled out variable can be accessed with the \Z5${nameOfTheVariable}\Z0 syntax):\Zn'
+    commandToBeExecuted="dialog --keep-tite --colors --ok-label 'Configure BaHaMAS'
+                                --cancel-label 'Abort setup'
+                                --backtitle 'BaHaMAS setup'
                                 --title 'BaHaMAS configuration'
+                                --separator '|'
                                 --form '$formHeader'
                          $dialogHeight $dialogWidth $formHeight"
     #Complete dialog box command
