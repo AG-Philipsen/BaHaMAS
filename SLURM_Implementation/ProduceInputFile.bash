483e6f4942a9989ec3a7ac40fe66485f4b97ecfe
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2020-02-14 15:49:08 +0100

    Add GPLv3 licence notice to all files

--- SLURM_Implementation/ProduceInputFile.bash
+++ SLURM_Implementation/ProduceInputFile.bash
@@ -1,3 +1,21 @@
+#
+#  Copyright (c)
+#
+#  This file is part of BaHaMAS.
+#
+#  BaHaMAS is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  BaHaMAS is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with BaHaMAS. If not, see <http://www.gnu.org/licenses/>.
+#
 
 function __static__AddToInputFile()
 {
