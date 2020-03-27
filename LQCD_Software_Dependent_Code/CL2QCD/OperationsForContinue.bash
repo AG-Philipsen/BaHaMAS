30a065b2297b9ce2a43c10ba2e3e04f039628767
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2020-03-27 14:48:12 +0100

    Better indent case code

--- LQCD_Software_Dependent_Code/CL2QCD/OperationsForContinue.bash
+++ LQCD_Software_Dependent_Code/CL2QCD/OperationsForContinue.bash
@@ -498,22 +498,70 @@ function __static__ModifyOptionsInInputFile()
     local oldString newString
     while [[ $# -gt 0 ]]; do
         case $1 in
-            startCondition=* )                  oldString="startCondition=[[:alpha:]]\+";                newString="startCondition=${1#*=}" ;;
-            initialConf=* )                     oldString="initialConf=[[:alnum:][:punct:]]*";           newString="initialConf=${1#*=}" ;;
-            initialPRNG=* )                     oldString="initialPRNG=[[:alnum:][:punct:]]*";           newString="initialPRNG=${1#*=}" ;;
-            hostSeed=* )                        oldString="hostSeed=[0-9]\+";                            newString="hostSeed=${1#*=}" ;;
-            intsteps0=* )                       oldString="integrationSteps0=[0-9]\+";                   newString="integrationSteps0=${1#*=}" ;;
-            intsteps1=* )                       oldString="integrationSteps1=[0-9]\+";                   newString="integrationSteps1=${1#*=}" ;;
-            f=* | checkpointEvery=* )           oldString="createCheckpointEvery=[0-9]\+";               newString="createCheckpointEvery=${1#*=}" ;;
-            F=* | confSaveEvery=* )             oldString="overwriteTemporaryCheckpointEvery=[0-9]\+";   newString="overwriteTemporaryCheckpointEvery=${1#*=}" ;;
-            m=* | measurements=* )              oldString="mcSteps=[0-9]\+";                             newString="mcSteps=${1#*=}" ;;  # This replacement works both with nHmcSteps and nRhmcSteps
-            measurePbp=* )                      oldString="measurePbp=[0-9]\+";                          newString="measurePbp=${1#*=}" ;;
-            useMP=* )                           oldString="useMP=[0-9]\+";                               newString="useMP=${1#*=}" ;;
-            kappaMP=* )                         oldString="kappaMP=[0-9]\+[.][0-9]\+";                   newString="kappaMP=${1#*=}" ;;
-            intsteps2=* )                       oldString="integrationSteps2=[0-9]\+";                   newString="integrationSteps2=${1#*=}" ;;
-            solverResiduumCheckEvery=* )        oldString="solverResiduumCheckEvery=[0-9]\+";            newString="solverResiduumCheckEvery=${1#*=}" ;;
-            nTimeScales=* )                     oldString="nTimeScales=[0-9]\+";                         newString="nTimeScales=${1#*=}" ;;
-            nPseudoFermions=* )                 oldString="nPseudoFermions=[0-9]\+";                     newString="nPseudoFermions=${1#*=}" ;;
+            startCondition=* )
+                oldString="startCondition=[[:alpha:]]\+"
+                newString="startCondition=${1#*=}"
+                ;;
+            initialConf=* )
+                oldString="initialConf=[[:alnum:][:punct:]]*"
+                newString="initialConf=${1#*=}"
+                ;;
+            initialPRNG=* )
+                oldString="initialPRNG=[[:alnum:][:punct:]]*"
+                newString="initialPRNG=${1#*=}"
+                ;;
+            hostSeed=* )
+                oldString="hostSeed=[0-9]\+"
+                newString="hostSeed=${1#*=}"
+                ;;
+            intsteps0=* )
+                oldString="integrationSteps0=[0-9]\+"
+                newString="integrationSteps0=${1#*=}"
+                ;;
+            intsteps1=* )
+                oldString="integrationSteps1=[0-9]\+"
+                newString="integrationSteps1=${1#*=}"
+                ;;
+            f=* | checkpointEvery=* )
+                oldString="createCheckpointEvery=[0-9]\+"
+                newString="createCheckpointEvery=${1#*=}"
+                ;;
+            F=* | confSaveEvery=* )
+                oldString="overwriteTemporaryCheckpointEvery=[0-9]\+"
+                newString="overwriteTemporaryCheckpointEvery=${1#*=}"
+                ;;
+            m=* | measurements=* )
+                oldString="mcSteps=[0-9]\+"
+                newString="mcSteps=${1#*=}"
+                ;;  # This replacement works both with nHmcSteps and nRhmcSteps
+            measurePbp=* )
+                oldString="measurePbp=[0-9]\+"
+                newString="measurePbp=${1#*=}"
+                ;;
+            useMP=* )
+                oldString="useMP=[0-9]\+"
+                newString="useMP=${1#*=}"
+                ;;
+            kappaMP=* )
+                oldString="kappaMP=[0-9]\+[.][0-9]\+"
+                newString="kappaMP=${1#*=}"
+                ;;
+            intsteps2=* )
+                oldString="integrationSteps2=[0-9]\+"
+                newString="integrationSteps2=${1#*=}"
+                ;;
+            solverResiduumCheckEvery=* )
+                oldString="solverResiduumCheckEvery=[0-9]\+"
+                newString="solverResiduumCheckEvery=${1#*=}"
+                ;;
+            nTimeScales=* )
+                oldString="nTimeScales=[0-9]\+"
+                newString="nTimeScales=${1#*=}"
+                ;;
+            nPseudoFermions=* )
+                oldString="nPseudoFermions=[0-9]\+"
+                newString="nPseudoFermions=${1#*=}"
+                ;;
             * )
                 Error "The option " emph "$1" " cannot be handled in the continue scenario.\n" "Simulation cannot be continued. The value " emph "beta = ${runId}" " will be skipped!"
                 BHMAS_problematicBetaValues+=( ${runId} )
