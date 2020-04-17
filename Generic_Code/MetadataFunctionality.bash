495cd11957802a5049e3983e0dc18c980e32b4c4
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2020-04-17 15:43:20 +0200

    Improve error report in case of wrong metadata

--- Generic_Code/MetadataFunctionality.bash
+++ Generic_Code/MetadataFunctionality.bash
@@ -19,8 +19,12 @@
 
 function ValidateParsedBetaValues()
 {
-    local modesThatRequireEntryInMetadataFile modesThatRequireNoEntryInMetadata\
-          runId software occurencesInFile
+    local modesThatRequireEntryInMetadataFile\
+          modesThatRequireNoEntryInMetadata\
+          runId software occurencesInFile\
+          idAbsent idNotAbsent idToBeAdded\
+          idMoreThanOnce
+    declare -A wrongSoftware
     modesThatRequireEntryInMetadataFile=(
         'continue'
         'continue-thermalization'
@@ -49,33 +53,82 @@ function ValidateParsedBetaValues()
     fi
 
     #Validate beta values: runId must appear at beginning of line!
+    idMoreThanOnce=()
+    idAbsent=()
+    wrongSoftware=()
+    idToBeAdded=()
+    idNotAbsent=()
     for runId in "${BHMAS_betaValues[@]}"; do
+        software=''
         occurencesInFile=$(grep -c "^${runId}" "${BHMAS_metadataFilename}" || true)
+        if [[ ${occurencesInFile} -gt 1 ]]; then
+            idMoreThanOnce+=( "${runId} (${occurencesInFile} occurrences)" )
+            continue
+        fi
         if ElementInArray ${BHMAS_executionMode#mode:} "${modesThatRequireEntryInMetadataFile[@]}"; then
             if [[ ${occurencesInFile} -eq 1 ]]; then
                 software=$(awk -v id="${runId}" '$1 == id {print $2}' "${BHMAS_metadataFilename}")
+                if [[ "${software}" != "${BHMAS_lqcdSoftware}" ]]; then
+                    wrongSoftware+=( ["${runId}"]="${software}" )
+                fi
             else
-                Fatal ${BHMAS_fatalLogicError}\
-                      'Run ID ' emph "${runId}" ' was found zero or several times in metadata file,\n'\
-                      'but it is needed to occur exactly one in ' emph "${BHMAS_executionMode#mode:}" 'mode.'
-            fi
-            if [[ "${software}" != "${BHMAS_lqcdSoftware}" ]]; then
-                Fatal ${BHMAS_fatalLogicError}\
-                      'Simulation with ID ' emph "${runId}" ' was run with ' emph "${software}"\
-                      '\nbut BaHaMAS has been asked to use ' emph "${BHMAS_lqcdSoftware}" ' as LQCD software!'
+                idAbsent+=( "${runId}" )
             fi
         else # here no occurence should be in file
+            idToBeAdded=()
             if [[ ${occurencesInFile} -eq 0 ]]; then
+                idToBeAdded+=( "${runId}" )
                 printf "%-40s%-25s# %s\n"\
                        "${runId}"\
                        "${BHMAS_lqcdSoftware}"\
                        "$(date +'%d.%m.%Y at %H:%M:%S')" >> "${BHMAS_metadataFilename}"
             else
-                Fatal ${BHMAS_fatalLogicError}\
-                      'Run ID ' emph "${runId}" ' was found in metadata file, but it should\n'\
-                      'not occure in ' emph "${BHMAS_executionMode#mode:}" 'mode.'
+                idNotAbsent+=( "${runId}" )
             fi
         fi
     done
-    cat $BHMAS_metadataFilename
+
+    #Make report and terminate if wrong metadata were found
+    if [[ ${#idMoreThanOnce[@]} -ne 0 ]]; then
+        Error -N 'The following run ID(s) was/were found in ' emph "${BHMAS_metadataFilename}" ' file\n'\
+              'several times and this should never happen (please adjust the file manually):'
+        for runId in "${idMoreThanOnce[@]}"; do
+            Error -n -N -e lo "   ${runId}"
+        done
+    fi
+    if [[ ${#idAbsent[@]} -ne 0 ]]; then
+        Error -N 'The following run ID(s) was/were not found in ' emph "${BHMAS_metadataFilename}" ' file,\n'\
+              'but exactly one occurrence should exist in ' emph "${BHMAS_executionMode#mode:}" ' execution mode:'
+        for runId in "${idAbsent[@]}"; do
+            Error -n -N -e lo "   ${runId}"
+        done
+    fi
+    if [[ ${#wrongSoftware[@]} -ne 0 ]]; then
+        Error -N 'The following simulation(s) was/were found in ' emph "${BHMAS_metadataFilename}" ' file, but\n'\
+              'was/were previously run with a different LQCD sofware than ' emph "${BHMAS_lqcdSoftware}" ':'
+        for runId in "${!wrongSoftware[@]}"; do
+            Error -n -N -e lo "   ${runId} (${wrongSoftware[${runId}]})"
+        done
+    fi
+    if [[ ${#idNotAbsent[@]} -ne 0 ]]; then
+        Error -N 'The following run ID(s) was/were found in ' emph "${BHMAS_metadataFilename}" ' file,\n'\
+              'but no occurrence should exist in ' emph "${BHMAS_executionMode#mode:}" ' execution mode:'
+        for runId in "${idNotAbsent[@]}"; do
+            Error -n -N -e lo "   ${runId}"
+        done
+        Error -N -e 'If you previously run BaHaMAS in this mode and it failed or got interrupted, adjust\n'\
+              'by hand the metadata file removing the line(s) reported above and run the script again.'
+    fi
+    if(( ${#idMoreThanOnce[@]} + ${#idAbsent[@]} + ${#wrongSoftware[@]} + ${#idNotAbsent[@]} > 0 )); then
+        Fatal ${BHMAS_fatalLogicError} 'Incorrect metadata were found.'
+    fi
+
+    if [[ ${#idToBeAdded[@]} -ne 0 ]]; then
+        for runId in "${idToBeAdded[@]}"; do
+            printf "%-40s%-25s# %s\n"\
+                   "${runId}"\
+                   "${BHMAS_lqcdSoftware}"\
+                   "$(date +'%d.%m.%Y at %H:%M:%S')" >> "${BHMAS_metadataFilename}"
+        done
+    fi
 }
